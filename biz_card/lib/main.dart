eb925e18ba7973dea3135b43a1cacf1c55adb664
Pekka Räsänen <5183385+pekomon@users.noreply.github.com>
2021-10-20 21:24:35 +0300

    Biz Card finalization

--- biz_card/lib/main.dart
+++ biz_card/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,8 +12,67 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white54,
-        body: Container(),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.red,
+                backgroundImage: AssetImage('images/saddog.jpg'),
+              ),
+              Text(
+                "Sad Dogger",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40.0,
+                    fontFamily: 'Pacifico'),
+              ),
+              Text(
+                "FLUTTER EXPERT",
+                style: TextStyle(
+                    color: Colors.tealAccent,
+                    fontSize: 26.0,
+                    fontFamily: 'SourceSansPro',
+                    letterSpacing: 2.0),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 160.0,
+                child: Divider(
+                  color: Colors.tealAccent,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.teal),
+                  title: Text(
+                    "+385 990 990 9900",
+                    style: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 20.0,
+                        fontFamily: 'SourceSansPro'),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 12.0, horizontal: 24.0),
+                child: ListTile(
+                  leading: Icon(Icons.email, color: Colors.teal),
+                  title: Text(
+                    "sad.dog@email.address",
+                    style: TextStyle(
+                        color: Colors.teal,
+                        fontSize: 20.0,
+                        fontFamily: 'SourceSansPro'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
