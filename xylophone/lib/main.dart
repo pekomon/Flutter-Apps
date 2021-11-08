11602e8b37da803ee12e6cf99101c775b8ed9b3d
Pekka Räsänen <5183385+pekomon@users.noreply.github.com>
2021-11-08 18:46:12 +0200

    Remove unneeded containers

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -2,16 +2,17 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const XylophoneApp());
+  runApp(XylophoneApp());
 }
 
 class XylophoneApp extends StatelessWidget {
-  const XylophoneApp({Key? key}) : super(key: key);
+  XylophoneApp({Key? key}) : super(key: key);
+
+  final player = AudioCache();
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final player = AudioCache();
     //final audioPlayer = AudioPlayer(playerId: 'my_unique_playerId');
     return MaterialApp(
       title: 'Xylophone',
@@ -32,75 +33,66 @@ class XylophoneApp extends StatelessWidget {
           child: Container(
             child: Column(
               children: [
-                Container(
-                  width: 100.0,
-                  height: 40.0,
-                  color: Colors.red,
-                  child: TextButton(
-                      onPressed: () {
-                        player.play('note1.wav');
-                      },
-                      child: const Text('')),
+                TextButton(
+                  onPressed: () {
+                    playSound(1);
+                  },
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.red)),
+                  child: const Text(''),
                 ),
-                Container(
-                  width: 100.0,
-                  height: 40.0,
-                  color: Colors.yellow,
-                  child: TextButton(
-                      onPressed: () {
-                        player.play('note2.wav');
-                      },
-                      child: const Text('')),
+                TextButton(
+                  onPressed: () {
+                    playSound(2);
+                  },
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.yellow)),
+                  child: const Text(''),
                 ),
-                Container(
-                  width: 100.0,
-                  height: 40.0,
-                  color: Colors.lightBlue,
-                  child: TextButton(
-                      onPressed: () {
-                        player.play('note3.wav');
-                      },
-                      child: const Text('')),
+                TextButton(
+                  onPressed: () {
+                    playSound(3);
+                  },
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.purple)),
+                  child: const Text(''),
                 ),
-                Container(
-                  width: 100.0,
-                  height: 40.0,
-                  color: Colors.tealAccent,
-                  child: TextButton(
-                      onPressed: () {
-                        player.play('note4.wav');
-                      },
-                      child: const Text('')),
+                TextButton(
+                  onPressed: () {
+                    playSound(4);
+                  },
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.tealAccent)),
+                  child: const Text(''),
                 ),
-                Container(
-                  width: 100.0,
-                  height: 40.0,
-                  color: Colors.pink,
-                  child: TextButton(
-                      onPressed: () {
-                        player.play('note5.wav');
-                      },
-                      child: const Text('')),
+                TextButton(
+                  onPressed: () {
+                    playSound(5);
+                  },
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.pink)),
+                  child: const Text(''),
                 ),
-                Container(
-                  width: 100.0,
-                  height: 40.0,
-                  color: Colors.amberAccent,
-                  child: TextButton(
-                      onPressed: () {
-                        player.play('note6.wav');
-                      },
-                      child: const Text('')),
+                TextButton(
+                  onPressed: () {
+                    playSound(6);
+                  },
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.amberAccent)),
+                  child: const Text(''),
                 ),
-                Container(
-                  width: 100.0,
-                  height: 40.0,
-                  color: Colors.greenAccent,
-                  child: TextButton(
-                      onPressed: () {
-                        player.play('note7.wav');
-                      },
-                      child: const Text('')),
+                TextButton(
+                  onPressed: () {
+                    playSound(7);
+                  },
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(Colors.greenAccent)),
+                  child: const Text(''),
                 ),
               ],
             ),
@@ -108,5 +100,9 @@ class XylophoneApp extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  void playSound(int soundNumber) {
+    player.play('note$soundNumber.wav');
   }
 }
